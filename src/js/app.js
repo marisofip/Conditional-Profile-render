093ffca59011230de0521eddffcf6220bb5da23c
marisofip <marisofipalencia@gmail.com>
2022-12-05 16:26:11 +0000

    agregando font awesome y target blank

--- src/js/app.js
+++ src/js/app.js
@@ -33,8 +33,8 @@ function render(variables = {}) {
   let nombre = variables.name === null ? "Lucy " : variables.name;
   let apellido = variables.lastname === null ? " Boilett " : variables.lastname;
   let rol = variables.role === null ? " Web developer " : variables.role;
-  /*let pais = variables.country === null ? " USA " : variables.country;
-  let ciudad = variables.city === null ? " Miami " : variables.city; ${ciudad} ${pais}*/
+  let pais = variables.country === null ? " USA " : variables.country;
+  let ciudad = variables.city === null ? " Miami " : variables.city;
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
@@ -42,12 +42,12 @@ function render(variables = {}) {
           <img src="${variables.avatarURL}" class="photo" />
           <h1> ${nombre} ${apellido} </h1>
           <h2> ${rol} </h2>
-          <h3> Miami, USA </h3>
+          <h3> ${ciudad} ${pais} </h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/4geeksacademy" target=”_blank”><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/4geeksacademy" target=”_blank”><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/4geeksacademy" target=”_blank”><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/4geeksacademy" target=”_blank”><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
